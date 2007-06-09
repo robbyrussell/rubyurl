25ec2c3d8019494f95088d9457ece1126ea4a755
Robby Russell <robby@planetargon.com>
2007-06-09 23:44:20 +0000

    Adding new action for links controller
    git-svn-id: https://svn.roundhaus.com/planetargon/rubyurl_2-0/trunk@13 f0bc2f90-cf15-dc11-b192-0016e6374d05

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,2 +1,16 @@
 class LinksController < ApplicationController
+
+  def new
+    @link = Link.find_or_create_by_website_url( params[:link][:website_url] )
+    @link.ip_address = request.remote_ip if @link.new_record?
+    
+    if @link.save
+      render :action => :show
+    else
+      flash[:warning] = 'There was an issue trying to create your RubyURL.'
+    end
+  end
+  
+  def show
+  end
 end
