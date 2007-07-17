d118b6830d73972cbd655ba277e3f98bb30be4bb
Robby Russell <robby@planetargon.com>
2007-07-17 01:28:45 +0000

    Modified routes for refactoring
    git-svn-id: https://svn.roundhaus.com/planetargon/rubyurl_2-0/trunk@28 f0bc2f90-cf15-dc11-b192-0016e6374d05

--- config/routes.rb
+++ config/routes.rb
@@ -23,6 +23,6 @@ ActionController::Routing::Routes.draw do |map|
   map.connect ':controller/:action/:id.:format'
   map.connect ':controller/:action/:id'
   
-  map.connect ':token', :controller => 'redirect', :action => 'index'
+  map.connect ':token', :controller => 'links', :action => 'redirect'
   
 end
