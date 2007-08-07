8b2dfb7696d28a45438331b1da52796f0163473e
Robby Russell <robby@planetargon.com>
2007-08-07 16:57:41 +0000

    a plethora of fixes... backwards compatibility... etc
    git-svn-id: https://svn.roundhaus.com/planetargon/rubyurl_2-0/trunk@52 f0bc2f90-cf15-dc11-b192-0016e6374d05

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -5,11 +5,12 @@ class LinksController < ApplicationController
   end
 
   def create
-    @link = Link.find_or_create_by_website_url( params[:link][:website_url] )
+    website_url = params.include?(:website_url) ? params[:website_url] : params[:link][:website_url]
+    @link = Link.find_or_create_by_website_url( website_url )
     @link.ip_address = request.remote_ip if @link.new_record?
     
     if @link.save
-      calculate_links # application controller
+      calculate_links # application controller, refactor soon
       render :action => :show
     else
       flash[:warning] = 'There was an issue trying to create your RubyURL.'
