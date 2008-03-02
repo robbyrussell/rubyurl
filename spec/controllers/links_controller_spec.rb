20157bf6ac66a72928a455a935a6e8e56bf6110e
Ryan McGeary <ryanongit@mcgeary.org>
2008-03-02 16:55:07 -0500

    Get the specs to pass; make the specs match the actual controller implementation.

--- spec/controllers/links_controller_spec.rb
+++ spec/controllers/links_controller_spec.rb
@@ -3,10 +3,19 @@ require File.dirname(__FILE__) + '/../spec_helper'
 describe LinksController, "index action" do
   controller_name :links
   
+  it "should redirect to the home action" do
+    get :index
+    response.should redirect_to( :action => 'home' )
+  end
+end
+
+describe LinksController, "home action" do
+  controller_name :links
+  
   before(:each) do
     @link = mock('link')
     Link.stub!(:new).and_return(@link)    
-    get :index
+    get :home
   end
   
   it "should render the index view" do
