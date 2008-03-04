4f14ea9a6a16a5c12a920ec7402e08ebe238e0b5
Robby Russell <robbyrussell@new-host-2.home.lan>
2008-03-03 20:10:56 -0800

    Adding a session secret...

--- config/environment.rb
+++ config/environment.rb
@@ -42,6 +42,8 @@ Rails::Initializer.run do |config|
   # config.active_record.default_timezone = :utc
   
   # See Rails::Configuration for more options
+  
+  config.action_controller.session = { :session_key => "_rubyurl_session", :secret => "oy8j2c45ync4ny43n3cmtho3cht35y4t8hn7" } 
 end
 
 # Add new inflection rules using the following format 
