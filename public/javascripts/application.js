ce1de142547579bc87147a26f35253f5da379208
Jeff Smick <sprsquish@gmail.com>
2008-05-14 09:50:05 -0700

    Add auto-copy functionality

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -19,3 +19,13 @@ function copy(rubyurl) {
 		$('x_copied').show();
 		
 }
+
+Event.observe(window, 'load', function() {
+  // Watch for user's click on "Auto Copy" checkbox
+  if ($('auto_copy'))
+    Event.observe('auto_copy', 'change', function(event) { document.cookie = 'auto_copy='+($F('auto_copy') ? 1 : 0); });
+
+  // Perform auto-copy if prudent
+  if ((link = $$('#url.arrow a')).length && document.cookie.match(/auto_copy=1/))
+    copy(link[0].href);
+});
