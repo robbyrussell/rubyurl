022af8407e952fd4cf517e8cc13e565d414fb2c0
Robby Russell <robby@planetargon.com>
2009-03-04 18:41:39 -0800

    Updating to use Clippy as a replacement to the non-Flash 10 compatible clipboard flash file. Had to remove the auto-copy due to incompatibility difference.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,8 +3,8 @@ module ApplicationHelper
   def clippy(text, bgcolor='#FFFFFF')
     html = <<-EOF
       <object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000"
-              width="110"
-              height="14"
+              width="80"
+              height="60"
               id="clippy" >
       <param name="movie" value="/clippy.swf"/>
       <param name="allowScriptAccess" value="always" />
@@ -13,8 +13,8 @@ module ApplicationHelper
       <param NAME="FlashVars" value="text=#{text}">
       <param name="bgcolor" value="#{bgcolor}">
       <embed src="/clippy.swf"
-             width="110"
-             height="14"
+             width="80"
+             height="60"
              name="clippy"
              quality="high"
              allowScriptAccess="always"
