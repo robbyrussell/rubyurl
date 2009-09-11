fe8f2ff4b94fde14bf3dc7a321a9f4971bc6fe92
Robby Russell <robby@planetargon.com>
2009-09-11 10:16:45 -0700

    Fixing view specs that were failing after last design changes were made.

--- spec/views/links/index_view_spec.rb
+++ spec/views/links/index_view_spec.rb
@@ -1,15 +1,15 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
 
-describe "links/index" do  
+describe "links/index" do
   before(:each) do
     render 'links/index'
   end
-  
+
   it "should display Create a RubyURL in a h1 tag" do
-    response.should have_tag('h1', 'Create a RubyURL')
+    response.should have_tag('label', 'Create a RubyURL')
   end
-  
-  it "should display a text input field for the user to paste their url in" do    
+
+  it "should display a text input field for the user to paste their url in" do
     response.should have_tag('input#link_website_url')
   end
 end
@@ -23,16 +23,16 @@ describe "links/show" do
     assigns[:link] = @link
     render 'links/show'
   end
-  
+
   it "should display Here is your RubyURL in a h1 tag" do
-    response.should have_tag('h1', 'Here is your RubyURL')
+    response.should have_tag('dt', 'Here is your RubyURL')
   end
-    
+
   it "should display a link for the user to copy" do
     response.should have_tag('div#url')
     response.should have_tag('a', 'http://localhost:3000/x093')
   end
-  
+
   it "should display the correct number of characters for the original URL (16)" do
     response.should have_tag('dd', '19 characters')
   end
