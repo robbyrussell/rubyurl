d3c893ebe6711221b44d676f28c09514186d36aa
Robby Russell <robby@planetargon.com>
2010-06-07 19:10:39 -0700

    Adding a quick workaround for Surbl checking of domains..

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,19 +1,23 @@
+require 'domainatrix'
+
 class Link < ActiveRecord::Base
   TOKEN_LENGTH = 4
-  
+
   has_many :visits
   has_many :spam_visits, :class_name => 'Visit', :conditions => ["flagged = 'spam'"]
-  
+
   validates_presence_of :website_url, :ip_address
-  validates_uniqueness_of :website_url, :token  
+  validates_uniqueness_of :website_url, :token
   validates_format_of :website_url, :with => /^(http|https):\/\/[a-z0-9]/ix, :on => :save, :message => 'needs to have http(s):// in front of it', :if => Proc.new { |p| p.website_url? }
-  
+
   before_create :generate_token
-  
+
+  validate :not_in_surbl
+
   def flagged_as_spam?
     self.spam_visits.empty? ? false : true
   end
-  
+
   def add_visit(request)
     visit = visits.build(:ip_address => request.remote_ip)
     visit.save
@@ -28,13 +32,20 @@ class Link < ActiveRecord::Base
       xml.tag!( :permalink, self.permalink )
     end
   end
-  
+
   def to_api_json
     self.to_json( :only => [ :website_url, :permalink ] )
   end
-  
+
   private
-  
+
+    def not_in_surbl
+      url = Domainatrix.parse(self.website_url)
+      dig_response = %x[/usr/bin/dig +short #{url.domain}.#{url.public_suffix}.multi.surbl.org]
+      RAILS_DEFAULT_LOGGER.warn("DEBUG: #{dig_response.inspect}")
+      errors.add_to_base("SPAM. Domain was found in surbl.") if dig_response =~ /127.0/i
+    end
+
     def generate_token
       if (temp_token = random_token) and self.class.find_by_token(temp_token).nil?
         self.token = temp_token
@@ -43,11 +54,11 @@ class Link < ActiveRecord::Base
         generate_token
       end
     end
-    
+
     def build_permalink
       self.permalink = DOMAIN_NAME + self.token
     end
-  
+
     def random_token
       characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890'
       temp_token = ''
